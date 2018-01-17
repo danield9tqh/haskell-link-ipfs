c7fe79bdf977a25908d14f063cc14752dee35c9d
Daniel <danield9tqh@me.com>
2018-01-17 15:29:01 -0800

    make dependencies in Main explicit

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Turtle
-import Filesystem.Path
-import Prelude hiding (FilePath)
+import qualified Turtle
 import IPFSDependencyFile (dependencyFile, ImportName, ImportHash, ImportStatement(Import, hash, name))
 import Text.Parsec (ParseError, parse, eof)
-import Text.Parsec.String
-import Data.Text (unpack, pack, concat)
-import Filesystem.Path.CurrentOS
+import qualified Text.Parsec.String
+import Data.Text (unpack, pack, concat, Text)
+import Filesystem.Path.CurrentOS as FS (directory, toText, fromText, replaceExtension, FilePath)
 
+parser :: Turtle.Parser FS.FilePath
+parser = Turtle.argPath "src" "The source file"
 
-parser :: Turtle.Parser FilePath
-parser = argPath "src" "The source file"
-
-getDependencyFile :: FilePath -> FilePath
+getDependencyFile :: FS.FilePath -> FS.FilePath
 getDependencyFile hsFile = replaceExtension hsFile "ipfs"
 
-getFileContent :: FilePath -> IO Text
+getFileContent :: FS.FilePath -> IO Turtle.Text
 getFileContent path = do
-    text <- readTextFile path
+    text <- Turtle.readTextFile path
     return text
 
 regularParse :: Text.Parsec.String.Parser a -> String -> Either ParseError a
@@ -28,7 +25,7 @@ regularParse p = parse p ""
 parseWithEof :: Text.Parsec.String.Parser a -> String -> Either ParseError a
 parseWithEof p = parse (p <* Text.Parsec.eof) ""
 
-parseFile :: Text -> [ImportStatement]
+parseFile :: Turtle.Text -> [ImportStatement]
 parseFile contents = case parseResult of
   Left  _      -> []
   Right result -> result
@@ -43,25 +40,23 @@ getName Import { name=name } = name
 getHashValues :: [ImportStatement] -> [ImportHash]
 getHashValues statements = (map getHash statements)
 
-changeName :: FilePath -> Text -> Maybe FilePath
+changeName :: FS.FilePath -> Turtle.Text -> Maybe FS.FilePath
 changeName originalPath newName = case originalPathDirectoryText of
   Left  _    -> Nothing
   Right dir -> Just (fromText (Data.Text.concat [dir, newName]))
   where originalPathDirectoryText = (toText (directory originalPath))
 
-ipfsToFile :: FilePath -> ImportStatement -> IO ()
+ipfsToFile :: FS.FilePath -> ImportStatement -> IO ()
 ipfsToFile srcFile importStatement = let
     dependencyHash = getHash importStatement
     destFile = changeName srcFile (pack ((getName importStatement) ++ ".hs"))
   in
     case destFile of
-      Nothing   -> echo "incorrect dest file"
-      Just dest -> output dest (inproc "ipfs" ["cat", pack dependencyHash] Turtle.empty)
+      Nothing   -> Turtle.echo "incorrect dest file"
+      Just dest -> Turtle.output dest (Turtle.inproc "ipfs" ["cat", pack dependencyHash] Turtle.empty)
 
 main = do
-    srcFile <- options "A utility for linking haskell dependencies from ipfs" parser
+    srcFile <- Turtle.options "A utility for linking haskell dependencies from ipfs" parser
     let dependencyFile = (getDependencyFile srcFile)
-    dependencyContents <- readTextFile dependencyFile
+    dependencyContents <- Turtle.readTextFile dependencyFile
     mapM_ id (map (ipfsToFile srcFile) (parseFile dependencyContents))
-    -- let dependencyHashes = getHashValues (parseFile dependencyContents)
-    -- stdout (inproc "ipfs" ["cat", pack (head dependencyHashes)] Turtle.empty)
